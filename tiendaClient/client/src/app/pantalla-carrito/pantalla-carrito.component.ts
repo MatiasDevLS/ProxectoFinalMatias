ddd634267100abc1bb7310f5216eef8ab936073e
Matias <matiaslopse@gmail.com>
2025-06-03 22:05:54 +0200

    añadido tienda

--- tiendaClient/client/src/app/pantalla-carrito/pantalla-carrito.component.ts
+++ tiendaClient/client/src/app/pantalla-carrito/pantalla-carrito.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from '../Models/Producto';
+import { ProductosService } from '../services/ProductoService/productos.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pantalla-carrito',
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './pantalla-carrito.component.html',
   styleUrl: './pantalla-carrito.component.scss'
 })
-export class PantallaCarritoComponent {
+export class PantallaCarritoComponent implements OnInit{
+  productos: Producto[] = []
+  size: number = 0;
+  cantidadSeleccionada: number = 1;
+  constructor(private productosService: ProductosService){
+
+  }
+
+  ngOnInit(): void {
+    var keys:string = localStorage.getItem('keysCarrito')!
+    keys = JSON.stringify(keys)
+    keys = keys.replace('"','')
+    keys = keys.replace('"','')
+
+    let arrayKeys = keys.split(',')
+
+    this.size = arrayKeys.length
+    arrayKeys.forEach((key: string) => {
+      this.productosService.getProducto(key).subscribe({
+        next: (producto:Producto)=> {
+          this.productos.push(producto)
+        }
+      })
+    });
+  }
+
 
 }
